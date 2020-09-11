03106e35bed971271b35972fa93838e42f7c7c00
arun.prabhu <arun.prabhu@mobilelive.ca>
2020-09-11 16:16:05 -0400

    Analyze Temperature Data

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -27,7 +27,11 @@ print("\nHouse sensor records for date: {} = {}".format(test_date.strftime("%m/%
 
 temperature_data = TemperatureData(data)
 recs = temperature_data.get_data_by_area(rec_area=test_area)
-print("\nHouse Temperature sensor records for area {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
+print("\nHouse Temperature sensor records for area {} = {}".format(test_area, len(recs)))
+print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
+
+recs = temperature_data.get_data_by_date(rec_date=test_date)
+print("\nHouse Temperature sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
 print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
 
 # Module 3 code here:
